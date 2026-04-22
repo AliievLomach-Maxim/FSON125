4f846fb39320254948dad9efdee3de2cbd542aa9
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2026-04-22 21:46:53 +0300

    +

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>Something</li>
+        <li>Something</li>
+        <li>Something</li>
+      </ul>
+    </footer>
+  )
+}
+export default Footer
