98e16942cd5b8bb2943a9ccd119ee7ec81ac89ff
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2026-05-08 21:03:40 +0300

    +

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,76 +1,11 @@
-import { useQuery } from '@tanstack/react-query'
-import { getArticles } from '../../services/articles'
-import { useState } from 'react'
+import OrderForm from '../OrderForm/OrderForm'
 
 const App = () => {
-  const [searchValue, setSearchValue] = useState('')
-  const [page, setPage] = useState(0)
-
-  const submit = (formData: FormData) => {
-    const textFromInputSearch = formData.get('search') as string
-    setSearchValue(textFromInputSearch)
-    setPage(0)
-  }
-
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['Articles', searchValue, page],
-    queryFn: () => getArticles({ page, searchValue }),
-    enabled: searchValue !== '',
-  })
-
-  // const handleChangePage = ({ selected }: { selected: number }) => {
-  //   setPage(selected)
-  // }
-  console.log('{data?.nbPages}', data?.nbPages)
   return (
-    <div>
-      <form action={submit}>
-        <input type='text' name='search' />
-        <button type='submit'>Search</button>
-      </form>
-      <hr />
-      <hr />
-      {isLoading && <h1>Loading...</h1>}
-      {isError && <h1>OOps some error...</h1>}
-      {data && data.hits.length > 0 && (
-        <ul>
-          {data.hits.map((article) => (
-            <li key={article.id}>
-              <a href={article.url}>{article.title}</a>
-            </li>
-          ))}
-        </ul>
-      )}
-      <hr />
-    </div>
+    <>
+      <OrderForm />
+    </>
   )
 }
 
 export default App
-
-// import { useQuery } from '@tanstack/react-query'
-// import { getSomePerson } from '../../services/api'
-// import { useState } from 'react'
-
-// const App = () => {
-//   const [counter, setCounter] = useState(0)
-
-//   const { data, isError, isLoading } = useQuery({
-//     queryKey: ['getPerson', 'getPerson2', counter],
-//     queryFn: () => getSomePerson(counter),
-//     enabled: counter >= 1,
-//   })
-
-//   // getSomePerson
-//   return (
-//     <div>
-//       <button onClick={() => setCounter(counter + 1)}>get person by ID: {counter}</button>
-//       <br />
-//       {isLoading && <p>loading...</p>}
-//       {isError && <p>oops...</p>}
-//       {JSON.stringify(data)}
-//     </div>
-//   )
-// }
-
-// export default App
